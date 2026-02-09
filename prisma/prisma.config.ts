afc9fdc32ca292b8e98893c503aa7a8b3efb3ba3
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-02-09 22:04:43 +0200

    reset

--- prisma/prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import { PrismaClientOptions } from "@prisma/client";
-
-export const prismaConfig: PrismaClientOptions = {
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-};
