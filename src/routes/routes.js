9ce23a8966905996dfc9e982edccb06834f0f4cf
Kenneth Siyabonga Ncube <160495796+ghost2468developer@users.noreply.github.com>
2026-02-10 08:34:48 +0200

    Create routes.js

---
+++ src/routes/routes.js
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import {
+  createTodo,
+  getTodos,
+  updateTodo,
+  deleteTodo
+} from '../controllers/controller.js'
+
+const router = Router()
+
+router.post('/', createTodo)
+router.get('/', getTodos)
+router.put('/:id', updateTodo)
+router.delete('/:id', deleteTodo)
+
+export default router
